de143e71d8cf7b6fa3aaba702bda7a52433d12a6
Dawid <dawid.okojew@o2.pl>
2024-01-24 17:51:13 +0100

    added number class  to number button

--- main.js
+++ main.js
@@ -3,25 +3,12 @@ const ac = document.querySelector('.ac')
 const de = document.querySelector('.de')
 const point = document.querySelector('.point')
 const slash = document.querySelector('.slash')
-const seven = document.querySelector('.seven')
-const eight = document.querySelector('.eight')
-const nine = document.querySelector('.nine')
 const multiplay = document.querySelector('.multiplay')
-const four = document.querySelector('.four')
-const five = document.querySelector('.five')
-const six = document.querySelector('.six')
 const subtraction = document.querySelector('.subtraction')
-const one = document.querySelector('.one')
-const two = document.querySelector('.two')
-const three = document.querySelector('.three')
 const addition = document.querySelector('.addition')
-const doubleZero = document.querySelector('.doubleZero')
-const zero = document.querySelector('.zero')
 const result = document.querySelector('.result')
+const numbers = document.querySelectorAll('.number')
 
 let content = ``
 
-seven.addEventListener('click', () => {
-	content = content + seven.textContent
-	display.textContent = content
-})
+
